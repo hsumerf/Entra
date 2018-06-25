f65391de149763171a974054f3cfbc538bed7f6b
H S Umer Farooq <h.s.umer.farooq@gmail.com>
2018-06-25 13:41:10 +0500

    after dressing files

---
+++ Documents/Visual Studio 2017/Projects/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime();
+            dt = DateTime.Now;
+            Console.WriteLine("hello"+ dt);
+            Console.Read();
+        }
+    }
+}
---
+++ Documents/Visual Studio 2017/Projects/ConsoleAppAssigment00/ConsoleAppAssigment00/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApplication1{
+
+    class Medication
+    {
+        public int Quantity { get; set; } = 30; // Has default value.
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Medication med = new Medication();
+            Console.WriteLine(med.Quantity);
+            med.Quantity *= 2;
+            Console.WriteLine(med.Quantity);
+        }
+    }
+}
